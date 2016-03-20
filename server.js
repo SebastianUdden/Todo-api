1a0a20a655cf2d58a2dd82669a0fbc8b062859fb
Sebastian Udden <sebastian.udden@gmail.com>
2016-03-20 22:11:52 +0100

    Final static todos

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=false&q=work
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -22,9 +22,15 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
-	} else {
-
 	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+	"Go to work on Saturday".indexOf('work');
+
 	res.json(filteredTodos);
 });
 
