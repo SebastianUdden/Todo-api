ff468e9a4bc39857a1ab58e38e7e79759c2460c6
Sebastian Udden <sebastian.udden@gmail.com>
2016-03-20 21:58:15 +0100

    Add variableplayground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/*var person = {
+	name: "Andrew",
+	age: 21
+};
+
+function updatePerson (obj) {
+	/*obj = {
+		name: 'Andrew',
+		age: 24
+	};*/
+/*	obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person);*/
+
+// Array Example
+
+var grades = [15, 37];
+
+
+function addGrade (gradesArray) {
+	gradesArray.push(40);
+	debugger;
+}
+
+
+addGrade(grades);
+console.log(grades);
